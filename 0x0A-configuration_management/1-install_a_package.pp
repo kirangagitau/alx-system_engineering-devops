6d8f6c70544fad0da7fcdac9dd8693217dcac2c7
kirangagitau <dennosea@gmail.com>
2024-06-29 01:51:17 +0300

    flask version 2.1.0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# install_flask.pp
-
-# Ensure the python3-pip package is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
 # Use the python::pip resource to install Flask version 2.1.0
+
 python::pip { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
